8fbf3e73c213d0decfb5dd358ab80cd627df24dd
Udovin <i.udovin@bars-open.ru>
2024-10-23 21:51:13 +0300

    dev: eslint apply

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>Это приложение на Vite + React</h1>
       <div className="card">
-      <p className="read-the-docs">
-          Пример базового count'ера. Каждый клик переназначает state на +1 значение
-      </p>
-        <button onClick={() => setCount((count) => count + 1)}>
+        <p className="read-the-docs">
+          Пример базового счетчика. Каждый клик переназначает state на +1
+          значение
+        </p>
+        <button type="button" onClick={() => setCount((prev) => prev + 1)}>
           count is {count}
         </button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/page.jsx
+++ src/page.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './global.css'
+import React, { StrictMode } from "react";
 
-createRoot(document.getElementById('root')).render(
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+import "./global.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
