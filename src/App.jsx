f8f90dbf1303430202cd627f63f35274425135e0
elen-oz <ozerova.lenka@gmail.com>
2024-10-26 12:53:16 +0200

    refactor: remove unnnes code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 
-import "./App.css";
-
 import "react-toastify/dist/ReactToastify.css";
-import { Counter } from "./components/Counter";
+import Login from "./pages/login.jsx";
 
 function App() {
   return (
     <>
-      <h1>Это приложение на Vite + React + MUI</h1>
-      <Counter />
+      <Login />
     </>
   );
 }
