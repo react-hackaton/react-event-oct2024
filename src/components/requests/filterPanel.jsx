01b47ec82eef20dc248db8869e5ab9b47b35c4b2
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-10-26 12:33:26 -0300

    Created filter functionality

--- src/components/requests/filterPanel.jsx
+++ src/components/requests/filterPanel.jsx
@@ -1,25 +1,128 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Box, Typography, Checkbox, TextField, Button } from "@mui/material";
+import { Box, Typography, Button, TextField } from "@mui/material";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+
+import HelpFilter from "./helperFilter.jsx";
 
 function FilterPanel() {
+  const [selectedDate, setSelectedDate] = useState(null);
+
   return (
-    <Box sx={{ width: "250px", padding: 2 }}>
-      <Typography variant="h6">Фильтрация</Typography>
-      {/* Add all your filter groups here with Checkbox and TextField components */}
-      <Box>
-        <Typography variant="subtitle1">Кому мы помогаем</Typography>
-        <Checkbox /> Пенсионеры
-        <Checkbox /> Дом престарелых
-        {/* Add more filter groups as needed */}
+    <Box
+      sx={{
+        width: "320px",
+        height: "947px",
+        padding: "20px",
+        backgroundColor: "#ffffff",
+      }}
+    >
+      <Typography
+        variant="h6"
+        sx={{
+          marginBottom: "25px",
+        }}
+      >
+        Фильтрация
+      </Typography>
+
+      <HelpFilter
+        title="Кому мы помогаем"
+        options={["Пенсионеры", "Дом престарелых"]}
+      />
+
+      <Box
+        sx={{
+          marginTop: "20px",
+        }}
+      >
+        <HelpFilter
+          title="Чем мы помогаем"
+          options={["Вещи", "Финансирование"]}
+        />
+
+        <Box
+          sx={{
+            paddingLeft: "42px",
+          }}
+        >
+          <Box
+            sx={{
+              height: "48px",
+              alignContent: "center",
+              border: "1px solid #000",
+            }}
+          >
+            <Typography
+              sx={{
+                fontSize: "14px",
+              }}
+            >
+              Волонтерство
+            </Typography>
+          </Box>
+
+          <Box
+            sx={{
+              backgroundColor: "#F5F5F5",
+            }}
+          >
+            <HelpFilter
+              title="Специализация"
+              options={["Квалифицированная", "Не требует профессии"]}
+            />
+            <HelpFilter
+              title="Формат"
+              options={["Онлайн", "Не требует профессии"]}
+            />
+            <HelpFilter title="Специализация" options={["Онлайн", "Офлайн"]} />
+            <HelpFilter
+              title="Необходимо волонтеров"
+              options={["Группа", "Один"]}
+            />
+          </Box>
+        </Box>
+        <Box>
+          <Typography
+            sx={{
+              fontSize: "14px",
+            }}
+          >
+            Помощь актуальна до:
+          </Typography>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DatePicker
+              label="Выберите дату"
+              value={selectedDate}
+              onChange={(newValue) => setSelectedDate(newValue)}
+              renderInput={(params) => (
+                <TextField
+                  fullWidth
+                  label={params.label}
+                  inputRef={params.inputRef}
+                  error={params.error}
+                  helperText={params.helperText}
+                  InputProps={params.InputProps}
+                  {...params.InputLabelProps}
+                />
+              )}
+            />
+          </LocalizationProvider>
+        </Box>
+        <Button
+          variant="outlined"
+          fullWidth
+          sx={{
+            borderRadius: "4px",
+            border: "1px solid #000",
+            mt: 2,
+            color: "#000000",
+          }}
+        >
+          СБРОСИТЬ
+        </Button>
       </Box>
-      <Box>
-        <Typography variant="subtitle1">Помощь актуальна до:</Typography>
-        <TextField type="date" fullWidth />
-      </Box>
-      <Button variant="outlined" fullWidth sx={{ mt: 2 }}>
-        СБРОСИТЬ
-      </Button>
     </Box>
   );
 }
--- src/components/requests/index.jsx
+++ src/components/requests/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Box, Grid } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 import FilterPanel from "./FilterPanel";
 
@@ -11,17 +11,57 @@ function Requests() {
         height: "100vh",
         width: "100wh",
         display: "flex",
-        justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "red",
+        justifyContent: "center",
       }}
     >
-      <Box sx={{ display: "flex", padding: 2 }}>
-        <FilterPanel />
-        <Box sx={{ flex: 1, paddingLeft: 4 }}>
-          {/* <SearchResultsHeader /> */}
-          <Box>
-            <div>Vertical Card</div>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          backgroundColor: "#F5F5F5",
+        }}
+      >
+        <Typography
+          variant="h4"
+          sx={{
+            textAlign: "start",
+            marginLeft: "40px",
+            marginTop: "30px",
+          }}
+        >
+          Запросы о помощи
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flex: 1,
+            margin: "0 auto",
+          }}
+        >
+          <Box sx={{ display: "flex", padding: 2 }}>
+            <FilterPanel />
+          </Box>
+
+          <Box sx={{ display: "flex", padding: 2 }}>
+            <Box
+              sx={{ flex: 1, paddingLeft: 4, width: "1080px", height: "149px" }}
+            >
+              {/* <SearchResultsHeader /> */}
+              <Box
+                sx={{ display: "flex", padding: 2, backgroundColor: "blue" }}
+              >
+                <div>Search</div>
+              </Box>
+
+              <Box
+                sx={{ display: "flex", padding: 2, backgroundColor: "blue" }}
+              >
+                <div>Vertical Card</div>
+              </Box>
+            </Box>
           </Box>
         </Box>
       </Box>
