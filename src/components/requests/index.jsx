17641b3873d21c7e461ef7340d4503caf07eea9e
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-10-26 09:57:16 -0300

    Created component Requests

---
+++ src/components/requests/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import {
+  Box,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+export function Requests() {
+  return (
+    <Box
+      sx={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box />
+    </Box>
+  );
+}
+
+export default Index;
