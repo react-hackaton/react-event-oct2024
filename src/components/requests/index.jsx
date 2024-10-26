48d404507ac40ee5a78fa5119da87c06f78e413e
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-10-26 10:42:38 -0300

    Added changes to Requests component

--- src/components/requests/index.jsx
+++ src/components/requests/index.jsx
@@ -1,27 +1,32 @@
 import React from "react";
 
-import {
-  Box,
-  IconButton,
-  InputAdornment,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { Box, Grid } from "@mui/material";
 
-export function Requests() {
+import FilterPanel from "./FilterPanel";
+
+function Requests() {
   return (
     <Box
       sx={{
         height: "100vh",
-        width: "100vw",
+        width: "100wh",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "red",
       }}
     >
-      <Box />
+      <Box sx={{ display: "flex", padding: 2 }}>
+        <FilterPanel />
+        <Box sx={{ flex: 1, paddingLeft: 4 }}>
+          {/* <SearchResultsHeader /> */}
+          <Box>
+            <div>Vertical Card</div>
+          </Box>
+        </Box>
+      </Box>
     </Box>
   );
 }
 
-export default Index;
+export default Requests;
