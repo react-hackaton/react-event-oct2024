81865d5fec473b1e7619fd58c4e3d98747a3c97e
RenataGaraeva <renatagaraeva12@gmail.com>
2024-10-27 14:51:24 +0300

    updated contacts.jsx

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,156 +1,214 @@
-import { Typography, List, ListItem, Box } from "@mui/material";
+import { Typography, List, ListItem, Box, Stack } from "@mui/material";
 
 function Contacts({ userInfo }) {
   return (
-    <Box
+    <Stack
       sx={{
-        display: "flex",
-        flexDirection: "column",
-        width: 260,
-        justifyContent: "space-between",
-        marginTop: 30
+        height: "982px",
       }}
     >
-      <Box>
-        <Typography
-          sx={{
-            fontFamily: "Roboto",
-            fontSize: 20,
-            fontWeight: 500,
-            lineHeight: 32,
-            letterSpacing: 0.15000000596046448,
-            textAlign: "left",
-            marginBottom: 10
-          }}
-        >
-          Email
-        </Typography>
-        <Typography
-          sx={{
-            fontFamily: "Roboto",
-            fontSize: 14,
-            fontWeight: 400,
-            lineHeight: 20.02,
-            letterSpacing: 0.17000000178813934,
-            textAlign: "left"
-          }}
-        >
-          {/*{userInfo.email}*/}
-          userInfo.email
-        </Typography>
-      </Box>
-      <Box>
-        <Typography
-          sx={{
-            fontFamily: "Roboto",
-            fontSize: 20,
-            fontWeight: 500,
-            lineHeight: 32,
-            letterSpacing: 0.15000000596046448,
-            textAlign: "left",
-            marginBottom: 10
-          }}
-        >
-          Телефон
-        </Typography>
-        <Typography
-          sx={{
-            fontFamily: "Roboto",
-            fontSize: 14,
-            fontWeight: 400,
-            lineHeight: 20.02,
-            letterSpacing: 0.17000000178813934,
-            textAlign: "left"
-          }}
-        >
-          {/*{userInfo.telephone}*/}
-          userInfo.telephone
-        </Typography>
-      </Box>
-      <Box
+      <Stack
         sx={{
           display: "flex",
           flexDirection: "column",
-          justifyContent: "space-between"
+          width: "260px",
+          height: "334px",
+          justifyContent: "flex-start",
+          alignItems: "stretch",
+          m: 0,
+          p: 0,
+          pt: 0,
         }}
       >
-        <Typography
+        <Stack
           sx={{
-            fontFamily: "Roboto",
-            fontSize: 20,
-            fontWeight: 500,
-            lineHeight: 32,
-            letterSpacing: 0.15000000596046448,
-            textAlign: "left",
-            marginBottom: 10
+            m: 0,
+            p: 0,
+            height: "62px",
           }}
         >
-          Социальные сети
-        </Typography>
+          <Typography
+            sx={{
+              fontSize: "20px",
+              fontWeight: "Normal",
+              textAlign: "left",
+              height: "32px",
+            }}
+          >
+            E-mail
+          </Typography>
+          <Typography
+            sx={{
+              fontSize: 14,
+              fontWeight: "Normal",
+              letterSpacing: "0.17000000178813934px",
+              textAlign: "left",
+              height: "20x",
+            }}
+          >
+            {/* {userInfo.email} */}
+            userInfo.email
+          </Typography>
+        </Stack>
+        <Box
+          sx={{
+            height: "62px",
+            marginTop: "30px",
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: "20px",
+              fontWeight: "Normal",
+              letterSpacing: "0.15000000596046448px",
+              textAlign: "left",
+              marginBottom: "10px",
+              height: "32px",
+            }}
+          >
+            Телефон
+          </Typography>
+          <Typography
+            sx={{
+              fontSize: "14px",
+              fontWeight: "Normal",
+              letterSpacing: "0.17000000178813934px",
+              textAlign: "left",
+              height: "20px",
+            }}
+          >
+            {/* {userInfo.telephone} */}
+            userInfo.telephone
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            height: "62px",
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: "20px",
+              fontWeight: "Normal",
+              letterSpacing: "0.15000000596046448px",
+              textAlign: "left",
+              height: "32px",
+              marginTop: "30px",
+            }}
+          >
+            Социальные сети
+          </Typography>
 
-        <List>
-          <ListItem>
-            {/*<a href={userInfo.socials.vk} style={{ display: "flex", alignItems: "center" }}>*/}
-            <a href="#" style={{ display: "flex", alignItems: "center" }}>›
-              <img src="../assets/vk.jpg" alt="Значок ВК" />
-              <Typography
-                sx={{
-                  fontFamily: "Roboto",
-                  fontSize: 20,
-                  fontWeight: 500,
-                  lineHeight: 32,
-                  letterSpacing: 0.15000000596046448,
-                  textAlign: "left",
-                  marginLeft: 32
+          <List
+            sx={{
+              height: "108px",
+            }}
+          >
+            <ListItem
+              sx={{
+                height: "36px",
+                m: 0,
+                p: 0,
+              }}
+            >
+              {/* <a href={userInfo.socials.vk} style={{ display: "flex", alignItems: "center" }}> */}
+              <a
+                href="#"
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  height: "24px",
                 }}
               >
-                Vkontakte
-              </Typography>
-            </a>
-          </ListItem>
+                <img
+                  src="src/assets/vk.jpg"
+                  alt="Значок ВК"
+                  style={{ height: "24px" }}
+                />
+                <Typography
+                  sx={{
+                    fontSize: "20px",
+                    letterSpacing: "0.15000000596046448px",
+                    textAlign: "left",
+                    height: "32px",
+                    marginLeft: "32px",
+                  }}
+                >
+                  Vkontakte
+                </Typography>
+              </a>
+            </ListItem>
 
-          <ListItem>
-            {/*<a href={userInfo.socials.telegram} style={{ display: "flex", alignItems: "center" }}>*/}
-            <a href='#' style={{ display: "flex", alignItems: "center" }}>
-              <img src="../assets/telegram.png" alt="Значок ТГ" />
-              <Typography
-                sx={{
-                  fontFamily: "Roboto",
-                  fontSize: 20,
-                  fontWeight: 500,
-                  lineHeight: 32,
-                  letterSpacing: 0.15000000596046448,
-                  textAlign: "left",
-                  marginLeft: 16
+            <ListItem
+              sx={{
+                height: "36px",
+                m: 0,
+                p: 0,
+              }}
+            >
+              {/* <a href={userInfo.socials.telegram} style={{ display: "flex", alignItems: "center" }}> */}
+              <a href="#" style={{ display: "flex", alignItems: "center" }}>
+                <img
+                  src="src/assets/telegram.png"
+                  alt="Значок ТГ"
+                  style={{ height: "24px" }}
+                />
+                <Typography
+                  sx={{
+                    fontSize: "20px",
+                    fontWeight: 500,
+                    letterSpacing: "0.15000000596046448px",
+                    textAlign: "left",
+                    marginLeft: "32px",
+                    height: "32px",
+                  }}
+                >
+                  Telegram
+                </Typography>
+              </a>
+            </ListItem>
+
+            <ListItem
+              sx={{
+                height: "36px",
+                m: 0,
+                p: 0,
+              }}
+            >
+              {/* <a href={userInfo.socials.whatsup} style={{ display: "flex", alignItems: "center" }}> */}
+              <a
+                href="#"
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  height: "24px",
                 }}
               >
-                Telegram
-              </Typography>
-            </a>
-          </ListItem>
-
-          <ListItem>
-            {/*<a href={userInfo.socials.whatsup} style={{ display: "flex", alignItems: "center" }}>*/}
-            <a href='#' style={{ display: "flex", alignItems: "center" }}>
-              <img src="../assets/whatsapp.png" alt="Значок Ватсапп" />
-              <Typography
-                sx={{
-                  fontFamily: "Roboto",
-                  fontSize: 20,
-                  fontWeight: 500,
-                  lineHeight: 32,
-                  letterSpacing: 0.15000000596046448,
-                  textAlign: "left",
-                  marginLeft: 16
-                }}
-              >
-                WhatsApp
-              </Typography>
-            </a>
-          </ListItem>
-        </List>
-      </Box>
-    </Box>
+                <img
+                  src="src/assets/whatsapp.png"
+                  alt="Значок Ватсапп"
+                  style={{ height: "24px" }}
+                />
+                <Typography
+                  sx={{
+                    fontSize: "20px",
+                    letterSpacing: "0.15000000596046448px",
+                    textAlign: "left",
+                    marginLeft: "32px",
+                    height: "32px",
+                  }}
+                >
+                  WhatsApp
+                </Typography>
+              </a>
+            </ListItem>
+          </List>
+        </Box>
+      </Stack>
+    </Stack>
   );
 }
 
