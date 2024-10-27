36fceda123eed8fae57a18e645c6ef29593bc933
elen-oz <ozerova.lenka@gmail.com>
2024-10-27 13:17:28 +0100

    feat: add toast to login error/suc

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,7 +2,16 @@ import axios from './axios';
 
 export const authAPI = {
   login: async (login, password) => {
-    const response = await axios.post('/auth', { login, password });
-    return response.data;
-  },
+    try {
+      const response = await axios.post('/auth', { login, password });
+      return response.data;
+    } catch (error) {
+      if (error.response?.status === 400) {
+        throw new Error('Неверный логин или пароль');
+      } else if (error.response?.status === 500) {
+        throw new Error('Сервер временно недоступен');
+      }
+      throw new Error('Произошла ошибка при входе');
+    }
+  }
 };
