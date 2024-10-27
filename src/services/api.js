088e992f2ac68f6f28c929258887e7840e2f8b1c
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-10-27 07:13:52 -0300

    Check

--- src/services/api.js
+++ src/services/api.js
@@ -1,27 +1,27 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "/natticharity.eveloth.ru",
+  baseURL: '/natticharity.eveloth.ru',
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
 
 export const fetchRequests = async () => {
   try {
-    const response = await api.get("/request");
+    const response = await api.get('/request');
     return response.data;
   } catch (error) {
-    console.error("Error fetching requests:", error);
+    console.error('Error fetching requests:', error);
     throw error;
   }
 };
 
 export const addToFavorites = async (requestId) => {
   try {
-    await api.post("/user/favourites", { requestId });
+    await api.post('/user/favourites', { requestId });
   } catch (error) {
-    console.error("Error adding to favorites:", error);
+    console.error('Error adding to favorites: error', error);
     throw error;
   }
 };
